ada00a9ff263b55628e1dbc09e7d3eab18778719
Shivangi Harsora <135323873+Sharsora@users.noreply.github.com>
2025-03-24 23:03:40 +0530

    Create backend.tf

---
+++ Project-1/app/backend.tf
@@ -0,0 +1,10 @@
+```sh
+terraform {
+  backend "s3" {
+    bucket = "learning-terraform"
+    key    = "backend/books_shop_app.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "dynamoDB-state-locking"
+  }
+}
+```
